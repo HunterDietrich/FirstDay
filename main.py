49bdcbc923463c043eff83b28bb9978fa805a978
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-05 00:28:03 +0000

    Added Conditionals

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ print("Hello " + user)
 
 color = input("What is your favorite color?")
 print("My favorite color is also" + color)
+
+# Conditionals - if/else statements used for desision-making
+
+# if color == "blue":
+    # print("the sky is blue")
+# elif color == "red":
+    # print("roses are red")
+# else:
+    # print("what?")
+
+ if type(color) == str:
+        print(Color is a string)
+
+if isinstance(color, str):
+    print(Color is an instance of a string)
+
+
+num = 10
+
+if is i
