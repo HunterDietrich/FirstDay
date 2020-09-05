0d79ce44bfb27fbaf3d34353277d64fdccd462fc
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-05 00:07:24 +0000

    Added variables

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Hello World and everybody else")
+
+# VARIABLE
+# interger
+score = 0
+health = 100
+name = "Link"
+# string data
+
+print(health)
+print(score)
+print("Hello " + name)
+
+user = input("What is your name?")
+print("Hello " + user)
+
+color = input("What is your favorite color?")
+print("My favorite color is also" + color)
